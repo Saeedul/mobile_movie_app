84134b22d086714118e33f00d1df05b28cc55856
Saeedul <karimsaeedul476@gmail.com>
2025-05-25 22:42:14 +0600

    fetching and displaying movies ongoing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,24 +1,45 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Image, ScrollView, Text, View } from "react-native";
-import { useRouter } from "expo-router";
+import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
+import { router, useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError
+  } = useFetch(() =>
+    fetchMovies({ query: '' }
+    ));
+
   return (
     <View className="flex-1 bg-primary">
-      <Image source={images.bg} className="absolute w-full z-0"/>
+      <Image source={images.bg} className="absolute w-full z-0" />
 
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-        <View className="flex-1 mt-5">
-          <SearchBar
-          onPress={() => router.push("/search")}
-          placeholder="Search for a movie"
+        {moviesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 seld-center"
           />
-        </View>
+        ) : moviesError ? (
+          <Text>Error: {moviesError?.message}</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => router.push("/search")}
+              placeholder="Search for a movie"
+            />
+          </View>
+        )}
 
       </ScrollView>
     </View>
